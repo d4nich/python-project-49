2505e4a680cea9cd2da8403932fdc26f3a3cf33a
Daniil Malyshev <m4lyshev@gmail.com>
2025-04-21 21:04:00 +0300

    Обновил prime формат вопроса пользователю

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,7 +2,7 @@ import random
 rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def get_round():
     num = random.randint(1, 50)
-    question = num
+    question = f'Question: {num}'
     for i in range(1, num):
         if num % i == 0 and i != num and i != 1:
             correct_answer = 'no'
