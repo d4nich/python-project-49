9d2c253040d0233ee796c041a693261107e9bd50
Daniil Malyshev <m4lyshev@gmail.com>
2025-04-22 23:17:29 +0300

    Переписана игра prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,12 +1,15 @@
 import random
 rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 def get_round():
     num = random.randint(1, 50)
     question = f'Question: {num}'
-    for i in range(1, num):
-        if num % i == 0 and i != num and i != 1:
-            correct_answer = 'no'
-            break
-        else:
-            correct_answer = 'yes'
+    correct_answer = 'yes' if is_prime(num) else 'no'
     return question, correct_answer
